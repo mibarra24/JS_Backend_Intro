f4457ce073b734972c180c0f418182c8a4ed1317
mibarra24 <marcodzeko@gmail.com>
2022-12-02 00:24:45 -0700

    docs updated

---
+++ AMARRE/app.js
@@ -0,0 +1,5 @@
+function moverPosicionRandom(elm) {
+    elm.style.position = 'absolute'
+    elm.style.top = Math.random() * (window.innerHeight - elm.offsetHeight) + 'px';
+    elm.style.left = Math.random() * (window.innerWidth - elm.offsetWidth) + 'px';
+}
